eb7f4330449a830c0480b63b63c293a318f24668
kanakakis <kanakakis@aueb.gr>
2015-06-29 15:24:36 +0300

    29_6

--- static/js/task.js
+++ static/js/task.js
@@ -14,6 +14,7 @@ var mycounterbalance = counterbalance;  // they tell you which condition you hav
 // All pages to be loaded
 var pages = [
 	"instructions/instruct-1.html",
+	"instructions/instruct-1a.html",
 	"instructions/instruct-2.html",
 	"instructions/instruct-3.html",
 	"instructions/instruct-4.html",
@@ -27,6 +28,7 @@ psiTurk.preloadPages(pages);
 
 var instructionPages = [ // add as a list as many pages as you like
 	"instructions/instruct-1.html",
+	"instructions/instruct-1a.html",
 	"instructions/instruct-2.html",
 	"instructions/instruct-3.html",
 	"instructions/instruct-4.html",
